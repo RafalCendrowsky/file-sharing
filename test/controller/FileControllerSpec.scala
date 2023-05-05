28ca846f1f3b636c9a80cfacb0dbe7ff3d9f042d
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2023-05-05 11:02:26 +0200

    refactor test

--- test/controller/FileControllerSpec.scala
+++ test/controller/FileControllerSpec.scala
@@ -4,14 +4,17 @@ import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.HttpHeader.ParsingResult.Error
 import akka.http.scaladsl.model.HttpMethods.GET
 import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.stream.alpakka.s3.ObjectMetadata
 import akka.stream.scaladsl.Source
 import akka.util.{ByteString, Timeout}
 import models.S3Client
 import org.mockito.MockitoSugar.when
+import org.openqa.selenium.InvalidArgumentException
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
+import play.api.http.HeaderNames.CONTENT_LENGTH
 import play.api.test.Helpers.{contentType, route, status, writeableOf_AnyContentAsEmpty}
 import play.api.test.{FakeRequest, Injecting}
 
@@ -31,17 +34,19 @@ class FileControllerSpec extends PlaySpec with GuiceOneAppPerSuite with Injectin
   "File controller" must {
 
     "download a file" in {
+      // This is a mock of the S3Client trait
       val s3Client = inject[S3Client]
-      val ec = inject[ExecutionContext]
 
       val key = "test-file.txt"
-      val headerVal: HttpHeader = HttpHeader.parse("Content-Length", "100") match {
+      val headerVal: HttpHeader =HttpHeader.parse(CONTENT_LENGTH, "100") match {
         case HttpHeader.ParsingResult.Ok(h, _) => h
-        case Error(error) => throw new RuntimeException(error.summary)
+        case Error(_) => throw new InvalidArgumentException("Invalid header")
       }
-      val source: Source[ByteString, Future[ObjectMetadata]] =
-        Source(List(ByteString("test"))).mapMaterializedValue(_ => Future.successful(ObjectMetadata(List(headerVal))))
 
+      val source: Source[ByteString, Future[ObjectMetadata]] =
+        Source.single(ByteString("test")).mapMaterializedValue(_ => Future.successful(ObjectMetadata(List(headerVal))))
+
+      // Mock file download
       when (s3Client.getObject(key)) thenReturn source
 
       val request = FakeRequest(GET.value, s"/api/v1/files/$key")
